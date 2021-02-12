85c158138ca4ffad01cf9da997c57b8dc70c8937
LeongYuXuan <S10204536@connect.np.edu.sg>
2021-02-12 22:48:21 +0800

    Fix ref issues: lesson1 ref wrong js

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,10 +1,11 @@
 $(document).ready(function (){
     var complete1 = false;
     //Send back to  lesson page with new thing
-    $("#lesson-1").on("click",function(){
+    $("#complete").on("click",function(){
+        console.log("test complete");
         complete1 = true;
         localStorage.setItem("complete1",JSON.stringify(complete1));
-        window.location.replace("lesson-1.html");
+        
     })
 
 
