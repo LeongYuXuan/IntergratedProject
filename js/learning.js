71205824d54485827ec1cea61c46b7580898d12e
LeongYuXuan <S10204536@connect.np.edu.sg>
2021-02-12 22:58:16 +0800

    Add code that change blank to medal if lesson complete

--- js/learning.js
+++ js/learning.js
@@ -1,4 +1,13 @@
 $(document).ready(function (){
+    //function to replace blank imgs with medal if complete
+    if (JSON.parse(localStorage.getItem("complete1")) == true){
+        $("#m1 img").attr('src',"../image/circuit-breaker.png")
+    }
+
+
+
+
+    //function to send user to lesson 1 page
     $("#lesson-1").on("click",function(){
         window.location.replace("lesson-1.html");
     })
