ef3ad38fc6bc8f4888b141b0277064fa0fc822a5
LeongYuXuan <S10204536@connect.np.edu.sg>
2021-02-16 22:13:41 +0800

    Adjust learning page to support more buttons

--- js/learning.js
+++ js/learning.js
@@ -4,11 +4,20 @@ $(document).ready(function (){
         $("#m1 img").attr('src',"../image/circuit-breaker.png")
     }
 
-    //function to send user to lesson 1 page
+    //event listener to send user to lesson 1
     $("#lesson-1").on("click",function(){
         window.location.replace("lesson-1.html");
     })
 
+    //event listener to send user to lesson 2 
+    $("#lesson-2").on("click",function(){
+        window.location.replace("lesson-2.html");
+    })
+
+    //event listener to send user to lesson 3
+    $("#lesson-3").on("click",function(){
+        window.location.replace("lesson-3.html");
+    })
 
 
 });
