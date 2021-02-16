e4a63889cd4f1ceb111c915cbce449955caaa058
LeongYuXuan <S10204536@connect.np.edu.sg>
2021-02-16 23:27:00 +0800

    Add nonworking js for input validate

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,4 +1,7 @@
 $(document).ready(function (){
+    //hide all response messages 
+    $(".response").hide();
+
     //var which would trigger medal give 
     var complete1 = false;
     
@@ -6,8 +9,11 @@ $(document).ready(function (){
     //code to check if thing is correct I guess
     
     
-    
-    
+    $("#q2").on("click",function(){
+        if ($(".question2 input[name = 'animal']:checked").val() == "donkey")
+        console.log(correct)
+
+    });
     
     //Finish lesson button.
     //Registers true for "complete" if all quizes done right 
