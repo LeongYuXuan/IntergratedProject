2027b9bbe35b7f94a8fbe8e9dcfaef4c2c076170
LeongYuXuan <S10204536@connect.np.edu.sg>
2021-02-18 19:30:52 +0800

    Add js to accomodate new content

--- js/learning.js
+++ js/learning.js
@@ -1,8 +1,43 @@
 $(document).ready(function (){
-    //function to replace blank imgs with medal if complete
+    //functions to replace blank imgs with medal if complete
+
+    //What is Covid-19?
     if (JSON.parse(localStorage.getItem("complete1")) == true){
         $("#m1 img").attr('src',"../image/circuit-breaker.png")
     }
+
+    //Safety Precautions
+    if (JSON.parse(localStorage.getItem("complete1")) == true){
+        $("#m1 img").attr('src',"../image/safety-precautions.png")
+    }
+
+    //SG Circuit Breaker precautions
+    if (JSON.parse(localStorage.getItem("complete1")) == true){
+        $("#m1 img").attr('src',"../image/circuit-breaker.png")
+    }
+
+    //What is Covid-19? Extra
+    if (JSON.parse(localStorage.getItem("complete1")) == true){
+        $("#m1 img").attr('src',"../image/circuit-breaker.png")
+    }
+
+    //How it became pandemic
+    if (JSON.parse(localStorage.getItem("complete1")) == true){
+        $("#m1 img").attr('src',"../image/covid-world.png")
+    }
+
+    //Damages by covid
+    if (JSON.parse(localStorage.getItem("complete1")) == true){
+        $("#m1 img").attr('src',"../image/circuit-breaker.png")
+    }
+
+
+
+
+
+    
+    
+    //Event listener for buttons
 
     //event listener to send user to lesson 1
     $("#lesson-1").on("click",function(){
